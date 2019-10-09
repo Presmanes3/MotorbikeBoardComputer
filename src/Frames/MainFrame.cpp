a7a536ee69c4236581d18a21849fd0ad07ee331f
Javier Presmanes Cardama <presmanes3@gmail.com>
2019-10-09 22:47:00 +0200

    Defined MainFrame

--- src/Frames/MainFrame.cpp
+++ src/Frames/MainFrame.cpp
@@ -1,7 +1,24 @@
 #include "MainFrame.h"
 
+#include "../ControllerActions/MainFrameActions.h"
+#include "../Managers/LcdManager.h"
+
 void MainFrame::draw( ) {
-  this->getManager( )->getScreen( )->drawStr(10, 10, F("Led Strip"));
+
+  switch(this->currentMachineState) {
+    case GlobalMenu::Frames::Main::ShowInformation:
+      this->getManager( )->getScreen( )->drawStr(10, 10, F("Time : "));
+      this->getManager( )->getScreen( )->drawStr(10, 20, F("Date : "));
+      this->getManager( )->getScreen( )->drawStr(10, 30, F("Speed : "));
+      this->getManager( )->getScreen( )->drawStr(10, 30,
+                                                 F("Acceleration X : "));
+      this->getManager( )->getScreen( )->drawStr(10, 30,
+                                                 F("Acceleration Y : "));
+      break;
+  }
+}
+
+/*this->getManager( )->getScreen( )->drawStr(10, 10, F("Led Strip"));
   this->getManager( )->getScreen( )->drawStr(10, 20, F("Accelerometer"));
   this->getManager( )->getScreen( )->drawStr(10, 30, F("Time/Date"));
 
@@ -17,6 +34,18 @@ void MainFrame::draw( ) {
     case GlobalMenu::Frames::Main::Time:
       this->getManager( )->getScreen( )->drawStr(2, 30, F(">"));
       break;
-  }
+  }*/
+void MainFrame::setup( ) {
+  Controller* tempController =
+      this->getManager( )->getLcdManager( )->getController( );
+
+  tempController->setFunctionality(new MainFrameAction::Up( ), UP);
+
+  tempController->setFunctionality(new MainFrameAction::Down( ), DOWN);
+
+  tempController->setFunctionality(new MainFrameAction::Click( ), CLICK);
+
+  tempController->setFunctionality(new MainFrameAction::Press( ), PRESS);
+
+  tempController->setFunctionality(new MainFrameAction::Null( ), RELEASE);
 }
-void MainFrame::setup( ) {}
